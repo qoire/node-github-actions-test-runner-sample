cb931a9792f149cbd43aa69418f10959cba7fc81
qoire <mr.sun.yao@gmail.com>
2022-11-11 16:11:44 -0500

    Initial tests, and tools

---
+++ src/some-generic-module.test.ts
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+import { summation } from "./some-generic-module";
+
+describe("some-generic-module unit tests", function () {
+  it("should sum 1+1=2", function () {
+    const result = summation(1, 1);
+    expect(result).to.eq(2);
+  });
+
+  it("should fail to sum 2+2=5", function () {
+    const result = summation(2, 2);
+    expect(result).to.eq(5);
+  });
+});
---
+++ src/some-generic-module.ts
@@ -0,0 +1,3 @@
+export const summation = (a: number, b: number): number => {
+  return a + b;
+}

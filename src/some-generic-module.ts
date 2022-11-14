1d903fb169a2967d273382cca4d990184beb622e
qoire <mr.sun.yao@gmail.com>
2022-11-14 17:37:31 -0500

    Add comments for subtract function

--- src/some-generic-module.ts
+++ src/some-generic-module.ts
@@ -2,6 +2,7 @@ export const summation = (a: number, b: number): number => {
   return a + b;
 }
 
+// This function subtracts two numbers :)
 export const subtraction = (a: number, b: number): number => {
   return a - b;
 }
